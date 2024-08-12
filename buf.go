1f88059cb34a978e0650569371828ca55ab50a31
cjijcb <cjijcb@gmail.com>
2024-08-12 15:57:44 +0800

    reader/writer function

--- buf.go
+++ buf.go
@@ -1,20 +1,50 @@
 package main
 
-
 import (
-	"fmt"
-	"net/http"
+    "io"
+    "net/http"
+    "os"
+	//"bufio"
 )
 
-func main(){
-
-	//buf := make([]byte, 1024)
- 
-	c := &http.Client{}
-
-	resp, _ := c.Get("https://example.com")
-
-	fmt.Println((*resp).Body)
-
+type MyReader struct {
+    *io.Reader
 }
+
+
+func main() {
+
+    c := &http.Client{}
+
+    resp, err := c.Get("https://example.com")
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+	rr := MyReader{bufio.NewReader(resp.Body)}
+
+	rr.byteTransferTo(os.Stdout)
+
+    //r := MyReader{resp.Body}
+    //r.byteTransferTo(os.Stdout)
+}
+
+func (r *MyReader) byteTransferTo(w io.Writer) {
+    buf := make([]byte, 1024)
+    for {
+        n, err := r.Read(buf)
+        if err != nil && err != io.EOF {
+            panic(err)
+        }
+        if n == 0 {
+            break
+        }
+
+        if _, err := w.Write(buf[:n]); err != nil {
+            panic(err)
+        }
+    }
+}
+
 
