51de3d3e42dd0ba8c2ccf6796da0d36cd1f8f812
cjijcb <cjijcb@gmail.com>
2024-08-14 13:16:15 +0800

    cleaned up

--- main.go
+++ main.go
@@ -1,86 +1,38 @@
 package main
 
-
 import (
-  "fmt"
-  //"io"
-  "bufio"
-  "os"
-  "time"
-  "net/http"
-
+    "net/http"
+    //"fmt"
+	"io"
+	"os"
 )
-
-
-type downloadedFile struct {
-	*os.File
-	Path string
-	Size int64
-}
-
-type client struct {
-	http.Client
-}
-
-
-type myResponse http.Response
+	
 
 func main() {
 
-	dF := downloadedFile{
-		Path: "file",
-	}
-	
-	dClient := &client{}
 
-	dClient.setTransport()
+	ch := connWorker()
 
-	fmt.Printf("%+v\n", (*dClient).Transport.(*http.Transport))
+	f, _ := os.Create("file")
+    defer f.Close()
 
-	myResponse, _ := dClient.Get("https://examplefile.com/file-download/48")
-
-	dF.File, _ = os.Create(dF.Path)
-
-	nR  := bufio.NewReader(myResponse.Body)
-
-	nR.WriteTo(dF.File)
-
-
-
-	//io.Copy(dF.File, rp.Body)
-
-        //fmt.Println(err)
-	
-  	//os.WriteFile(dF.Path, b, 0666)
-
-	fi , _ := os.Stat(dF.Path)
-	
-	dF.Size = fi.Size()
-
-	fmt.Println(dF.Size)
-}
-
-
-func (r myResponse) Read(p []byte) (n int, err error){
-	fmt.Println("this")
-	return 0, nil
-
-}
-
-func (c *client) setTransport() {
-  
-	tr := &http.Transport{
-		MaxIdleConns:       16,
-		MaxConnsPerHost:    16,
-		IdleConnTimeout:    60 * time.Second,
-		DisableCompression: true,
-	}
-	
-	c.Transport = tr
+	f.ReadFrom(<-ch)
 
 }
 
 
+func connWorker() chan io.Reader {
 
+	ch := make(chan io.Reader)
 
+    c := &http.Client{}
+
+	go func() {
+		resp, _ := c.Get("https://example.com")
+		ch <- resp.Body
+	}()
+
+	return ch
+
+}
 
