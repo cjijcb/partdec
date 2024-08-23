7972108b8a5afbd1217b936887991228c391932b
cjijcb <cjijcb@gmail.com>
2024-08-23 20:07:30 +0800

    bytePartitioning added

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 
 	chR := make(chan io.ReadCloser)
 
-	fmt.Println(getRawURL(os.Args))
+	//fmt.Println(getRawURL(os.Args))
 
 	rawURL := "http://ipv4.download.thinkbroadband.com/5MB.zip"
 
@@ -41,12 +41,14 @@ func main() {
 	nc := buildNetconn(ct, req)
 	headers, contentLength := getHeaders(nc)
 
+	buildBytePartition(contentLength, 3)
+
 	fileName := buildFileName(rawURL, &headers)
 	file := buildFile(fileName)
 
-	fmt.Println("c:", contentLength)
+	//fmt.Println("c:", contentLength)
 	//fmt.Println("hd:", hd )
-	fmt.Println(fileName)
+	//fmt.Println(fileName)
 
 	wg.Add(3)
 
@@ -70,14 +72,16 @@ func buildNetconn(ct *http.Client, req *http.Request) *Netconn {
 }
 
 func buildFileName(rawURL string, hdr *http.Header) string {
-	_, params, _ := mime.ParseMediaType(hdr.Get("Content-Disposition"))
+	_, params, err := mime.ParseMediaType(hdr.Get("Content-Disposition"))
+	doHandle(&err)
 	fileName := params["filename"]
 
 	if fileName != "" {
 		return fileName
 	}
 
-	url, _ := url.Parse(rawURL)
+	url, err := url.Parse(rawURL)
+	doHandle(&err)
 
 	fileName = path.Base(url.Path)
 	return fileName
@@ -88,7 +92,7 @@ func buildFile(name string) *FileXtd {
 
 	f, err := os.OpenFile(name, os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0666)
 	
-	handle(&err)
+	doHandle(&err)
 
 	file := &FileXtd{
 		File:         f,
@@ -100,11 +104,13 @@ func buildFile(name string) *FileXtd {
 }
 
 func buildReq(method string, rawURL string) *http.Request {
-	req, _ := http.NewRequest(method, rawURL, nil)
+	req, err := http.NewRequest(method, rawURL, nil)
+	doHandle(&err)
 	req.Proto = "http/1.1"
 	req.ProtoMajor = 1
 	req.ProtoMinor = 1
 	req.Header.Set("Accept", "*/*")
+	//req.Header.Set("Range", "bytes=1024-6000")
 	req.Header.Set("User-Agent", "fssn/1.0.0")
 	return req
 }
@@ -120,8 +126,8 @@ func buildClient() *http.Client {
 func doConn(nc *Netconn, chR chan io.ReadCloser) {
 	defer wg.Done()
 	resp, err := nc.Client.Do(nc.Request)
-	handle(&err)
-	fmt.Println(nc.Request)
+	doHandle(&err)
+	//fmt.Println(nc.Request)
 	//defer resp.Body.Close()
 	chR <- resp.Body
 }
@@ -144,9 +150,7 @@ func doWriteFile(f *FileXtd, chR chan io.ReadCloser) {
 
 func getFileSize(f *FileXtd) int64 {
 	fi, err := f.Stat()
-	if err != nil {
-		fmt.Println(err)
-	}
+	doHandle(&err)
 	return fi.Size()
 }
 
@@ -163,8 +167,29 @@ func getRawURL(a []string) string {
 	return a[len(a)-1]
 }
 
-func handle(err *error) {
+func doHandle(err *error) {
 	if *err != nil {
 		log.Println(*err)
 	}
 }
+
+
+func buildBytePartition(byteCount int64, parts int) {
+
+    // +1 because zero is included
+	partSize := int(byteCount + 1) / parts
+
+    for i, j := 1, 0; i <= parts; i, j = i+1, j+partSize {
+
+        lowerbound := j
+        upperbound := lowerbound + partSize - 1
+        if i == parts {
+            upperbound = int(byteCount)
+        }
+
+        r := fmt.Sprintf("bytes=%v-%v", lowerbound, upperbound)
+        fmt.Println(r)
+
+    }
+
+} 
