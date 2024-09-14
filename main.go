884a80aaa4708e43801de9ab8124845a00657aaf
cjijcb <cjijcb@gmail.com>
2024-09-14 21:23:56 +0800

    fixed isFile isDir functions nil pointer deref error

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import ()
 func main() {
 
 	const FileNumParts int = 3
-	//URI := "http://ipv4.download.thinkbroadband.com/5MB.zip"
-	URI := "trusrc.dat"
+	URI := "http://ipv4.download.thinkbroadband.com/5MB.zip"
+	//URI := "trusrc.dat"
 	dstDirs := []string{"dir1/","dir2/"}
 
 	d, err  := buildDownload(FileNumParts, dstDirs, URI)
