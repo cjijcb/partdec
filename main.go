7dfe53b9e24f1183d8f99c17d921d76c4076f875
cjijcb <cjijcb@gmail.com>
2024-09-16 13:10:25 +0800

    file.go conn.go renamed. error handling refactored

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ func main() {
 	const FileNumParts int = 3
 	URI := "http://ipv4.download.thinkbroadband.com/5MB.zip"
 	//URI := "trusrc.dat"
-	dstDirs := []string{"dir1/","dir2/"}
+	dstDirs := []string{"dir1/", "dir2/"}
 
 	d, err := buildDownload(FileNumParts, dstDirs, URI)
 	doHandle(err)
