e8927f71a72ff6ed69742711314e187d124d8c4e
cjijcb <cjijcb@gmail.com>
2024-10-31 12:33:23 +0800

    -size flag added

--- input.go
+++ input.go
@@ -6,27 +6,65 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"unicode"
 )
 
 type (
 	ErrBuilder struct {
 		*strings.Builder
 	}
-	FlagMultiStr []string
+	Paths    []string
+	ByteSize int64
+)
+
+const (
+	Kibi = 1024
+	Mebi = 1024 * 1024
+	Gibi = 1024 * 1024 * 1024
+	Tebi = 1024 * 1024 * 1024 * 1024
+
+	Kilo = 1000
+	Mega = 1000 * 1000
+	Giga = 1000 * 1000 * 1000
+	Tera = 1000 * 1000 * 1000 * 1000
 )
 
 var (
-	PartFlag   int
-	DirFlag    FlagMultiStr
+	PartFlag   int      = 1
+	SizeFlag   ByteSize = -1
+	DirFlag    Paths
 	ErrOnFlags = &ErrBuilder{new(strings.Builder)}
+
+	ByteUnit = map[string]int64{
+		"":  1,
+		"B": 1,
+
+		"KIB": Kibi,
+		"MIB": Mebi,
+		"GIB": Gibi,
+		"TIB": Tebi,
+
+		"K": Kibi,
+		"M": Mebi,
+		"G": Gibi,
+		"T": Tebi,
+
+		"KB": Kilo,
+		"MB": Mega,
+		"GB": Giga,
+		"TB": Tera,
+	}
 )
 
-func maintest() {
+func main() {
 
 	flag.CommandLine.SetOutput(ErrOnFlags)
 
 	flag.Var(&DirFlag, "dir", "Specify directories (can be used times)")
+
+	flag.Var(&SizeFlag, "size", "Specify directories (can be used times)")
 
 	flag.IntVar(&PartFlag, "part", 1, "help message")
 
@@ -40,6 +78,7 @@ func maintest() {
 
 	fmt.Println("part value is: ", PartFlag)
 	fmt.Println("Directories:", DirFlag)
+	fmt.Println("Size:", SizeFlag)
 
 }
 
@@ -47,12 +86,43 @@ func (e *ErrBuilder) Error() string {
 	return e.String()
 }
 
-func (fms *FlagMultiStr) String() string {
-	return strings.Join(*fms, ",")
+func (ps *Paths) String() string {
+	return strings.Join(*ps, ",")
 }
 
-func (fms *FlagMultiStr) Set(value string) error {
-	*fms = append(*fms, value)
+func (ps *Paths) Set(value string) error {
+	*ps = append(*ps, value)
+	return nil
+}
+
+func (bs *ByteSize) String() string {
+	return fmt.Sprintf("%d", bs)
+}
+
+func (bs *ByteSize) Set(value string) error {
+
+	unitStr := strings.TrimLeftFunc(
+		value,
+		func(r rune) bool { return unicode.IsNumber(r) },
+	)
+
+	byteStr := strings.TrimRightFunc(
+		value,
+		func(r rune) bool { return !unicode.IsNumber(r) },
+	)
+
+	multiplier, found := ByteUnit[strings.ToUpper(unitStr)]
+
+	if !found {
+		return fmt.Errorf("parse error")
+	}
+
+	if byteCount, err := strconv.ParseInt(byteStr, 10, 64); err == nil {
+		*bs = ByteSize(byteCount * multiplier)
+	} else {
+		return err
+	}
+
 	return nil
 }
 
